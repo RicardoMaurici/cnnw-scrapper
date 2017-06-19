833927fef2734be792272769bb9c6cd266696407
Ricardo Maurici <ricardo.maurici@hotmail.com>
2017-06-19 14:23:18 -0300

    db models

--- cnnw/webclient/models.py
+++ cnnw/webclient/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+	name = models.CharField(max_length=30)
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=70,blank=False)
+    password = models.CharField(max_length=100)
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    companies = models.ManyToManyField(Company)
+    persons = models.ManyToManyField(Person)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
