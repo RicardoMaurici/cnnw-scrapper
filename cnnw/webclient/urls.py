e2c4e86278316a7ee0f479a08881428050c480de
Ricardo Maurici <ricardo.maurici@hotmail.com>
2017-06-19 16:16:04 -0300

    views and urls

--- cnnw/webclient/urls.py
+++ cnnw/webclient/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^config/$', views.config, name='config'),
 ]
--- cnnw/webclient/views.py
+++ cnnw/webclient/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Index CnNw.")
+
+def search(request,search_id):
+    return HttpResponse("search CnNw.")
+
+def config(request):
+    return HttpResponse("Config CnNw.")
