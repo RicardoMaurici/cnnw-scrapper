414edecae1d3dcfac375d2a59f7fd054e5b17c1b
Matheus Frata <msfrata@hotmail.om>
2017-06-25 14:31:24 -0300

    removing users-app folders

--- cnnw/webclient/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebclientConfig(AppConfig):
-    name = 'webclient'
--- cnnw/webclient/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^search/$', views.search, name='search'),
-    url(r'^config/$', views.config, name='config'),
-]
