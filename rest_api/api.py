76c91038bd85c6f8d2ceac2769908fc72a38b3a8
Matheus Frata <msfrata@hotmail.om>
2017-06-27 20:48:26 -0300

    post function on test collection #19

--- rest_api/api.py
+++ rest_api/api.py
@@ -11,7 +11,7 @@ api.config['MONGO_URI'] = 'mongodb://heroku_h5f5b7fv:h6hgi06v9nc0asucuj93h5dn4a@
 
 mongo = PyMongo(api)
 
-@api.route('/', methods=['GET'])
+@api.route('/api', methods=['GET'])
 def get_all_tests():
     tests = mongo.db.test
 
@@ -20,6 +20,44 @@ def get_all_tests():
         output.append({'name':q['name'], 'number': q['number'], 'extra_field':q['extra_field']})
 
     return jsonify({'result': output})
+
+
+@api.route('/api/<name>', methods=['GET'])
+def get_one_tests(name):
+    tests = mongo.db.test
+
+    document = tests.find_one({'name':name})
+
+    if document:
+        output = {'name':document['name'], 'number': document['number'], 'extra_field':document['extra_field']}
+    else:
+        output = 'No results Found'
+
+    return jsonify({'result': output})
+
+@api.route('/api/<name>', methods=['POST'])
+def post_one_tests(name):
+    tests = mongo.db.test
+
+    newDocummentName = name
+    newDocummentNumber = request.json['number']
+    newDocummentExtra_field = request.json['extra_field']
+
+    newDocummentId = tests.insert({'name': newDocummentName,
+                                   'number': newDocummentNumber,
+                                   'extra_field':newDocummentExtra_field
+                                   })
+
+    output = None
+    if tests.find_one({'_id': newDocummentId }):
+        output = 'Error while inserting'
+    else:
+        output = 'Inserted'
+
+    return jsonify({'result' : output})
+
+
+
 
 
 if __name__ == '__main__':
