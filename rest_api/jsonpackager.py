2949ae176343689e3374a9fde42de240f312514e
Matheus Frata <msfrata@hotmail.om>
2017-07-01 16:33:36 -0300

    documentation of jsonpackager

--- rest_api/jsonpackager.py
+++ rest_api/jsonpackager.py
@@ -1,7 +1,19 @@
 from flask import jsonify
 
 def jsonRaw(raw):
+    """
+    Takes the raw input and return in the json format
 
+    Parameters
+    ----------
+    raw : cursor object or dict
+
+    Returns
+    -------
+    json
+        json of the dicts
+
+    """
     output = []
     if type(raw) is dict:
         output = format_dict(raw)
@@ -11,6 +23,18 @@ def jsonRaw(raw):
     return jsonify({'result': output})
 
 def format_dict(raw_dict):
+    """
+    Takes the raw dict and return a f formated one
+
+    Parameters
+    ----------
+    raw_dict : dict
+
+    Returns
+    -------
+    dict
+        formated dict
+    """
     return {'name':raw_dict['name'],
             'number': raw_dict['number'],
             'extra_field':raw_dict['extra_field']
