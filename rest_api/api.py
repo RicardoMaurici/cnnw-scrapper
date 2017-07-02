daae64bc2972a7035cf04fdb0a947426e6234d91
Matheus Frata <msfrata@hotmail.om>
2017-07-01 21:09:09 -0300

    get all and post one in the news route

--- rest_api/api.py
+++ rest_api/api.py
@@ -1,9 +1,11 @@
-from flask import Flask, Response
-from flask import request
-from flask import make_response as apiResponse
-from jsonpackager import jsonRaw
-from flask_pymongo import PyMongo
+# encoding: utf-8
+# encoding: iso-8859-1
+# encoding: win-1252
 
+from flask import Flask, Response, jsonify, request
+from flask import make_response as apiResponse
+from jsonpackager import jsonRaw, format_dict, API_DATA_KEYS
+from flask_pymongo import PyMongo
 
 api = Flask(__name__)
 
@@ -12,37 +14,26 @@ api.config['MONGO_URI'] = 'mongodb://heroku_h5f5b7fv:h6hgi06v9nc0asucuj93h5dn4a@
 
 mongo = PyMongo(api)
 
-@api.route('/api', methods=['GET'])
-def get_all_tests():
-    document = mongo.db.test.find()
-    return apiResponse(jsonRaw(document))
+@api.route('/api/v1/news', methods=['GET', 'POST', 'OPTIONS'])
+def api_news():
+    if request.method == 'GET':
+        all_documents = mongo.db.news.find()
+        return apiResponse(jsonRaw(all_documents))
 
+    elif request.method == 'POST':
+        news_collection = mongo.db.news
+        newDocummentId = news_collection.insert(format_dict(request.json))
+        print newDocummentId
+        if news_collection.find_one({'_id': newDocummentId }):
+            return apiResponse(jsonify({'_id': str(newDocummentId) }), 201)
+        else:
+            return apiResponse('', 400)
 
-@api.route('/api/<name>', methods=['GET'])
-def get_one_tests(name):
-    document = mongo.db.test.find_one({'name':name})
-    return apiResponse(jsonRaw(document))
+    elif request.method == 'OPTIONS':
+        return "options"
 
-@api.route('/api/<name>', methods=['POST'])
-def post_one_tests(name):
-    tests = mongo.db.test
-
-    newDocummentName = name
-    newDocummentNumber = request.json['number']
-    newDocummentExtra_field = request.json['extra_field']
-
-    newDocummentId = tests.insert({'name': newDocummentName,
-                                   'number': newDocummentNumber,
-                                   'extra_field':newDocummentExtra_field
-                                   })
-
-    output = None
-    if tests.find_one({'_id': newDocummentId }):
-        output = 'Error while inserting'
     else:
-        output = 'Inserted'
-
-    return jsonify({'result' : output})
+        return apiResponse('',405)
 
 if __name__ == '__main__':
     api.run(debug=True)
