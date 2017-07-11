2cbacd9850f50f3b01a00c6ad589b532c815b721
Matheus Frata <msfrata@hotmail.om>
2017-07-10 23:45:34 -0300

    add the predict functions

--- predictor/entity.py
+++ predictor/entity.py
@@ -1,8 +1,16 @@
-from tablelize_data import *
+import spacy
+#import function of english translation
 
-def predictEntity(string_arg):
+#instalation of the required package
+os.system("python -m spacy download en")
+
+#globals
+encoding = 'utf-8'
+nlp = spacy.load('en')
+
+def predictPeople(string_arg):
     """
-    Predicts the entities of a news based on the news body
+    Predicts the people of a news based on the news body
     ---
     Args: news body
         String
@@ -10,4 +18,26 @@ def predictEntity(string_arg):
     Return: the entities
         List of strings
     """
-    pass
+    #add the translation function
+    #string_arg = translate(string_arg)
+    doc = nlp(unicode(string_arg, encoding))
+    output =  [ent.text for ent in doc.ents if ent.label_ == 'PERSON']
+    return list(set(output))
+
+def predictCompanies(string_arg):
+    """
+    Predicts the companies of a news based on the news body
+    ---
+    Args: news body
+        String
+    ---
+    Return: the entities
+        List of strings
+    """
+    #add the translation function
+    #string_arg = translate(string_arg)
+    doc = nlp(unicode(string_arg, encoding))
+    output =  [ent.text for ent in doc.ents if ent.label_ == 'ORG']
+    output = list(set(output))
+    #output = [translate(company) for company in output]
+    return output
