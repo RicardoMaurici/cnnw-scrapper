67b7789ca385698621be98c0929ec08bfe300963
Matheus Frata <msfrata@hotmail.om>
2017-07-12 11:43:02 -0300

    add algorithm skeleton for process_data

--- news_bot/data_manipulation.py
+++ news_bot/data_manipulation.py
@@ -1,5 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import pytz
+
+
+
 class ProcessData(object):
     def process_data(self, data):
-    	print(data)
+        news = dict(data)
+
+        #verify if url exists in the mongo db
+
+        #concatenate headline and body and remove headline key
+
+        #trasnform body into a big string
+
+        #copy the body to another var and remove accents, characters and extra spaces
+
+        #predic entities
+
+        #predict risk
+
+        #predic category
+
+        #predic tags
+
+        #get published data and delete date key
+
+        #get scraped date
+
+        #store in the mongodb
