aeba95323801a76267978ee264ea5578c0c21bae
Matheus Frata <msfrata@hotmail.om>
2017-07-12 23:07:06 -0300

    add scripts to run in heroku

---
+++ install.py
@@ -0,0 +1,5 @@
+import os
+
+#instalation of the required package
+print "Installing 'en' package."
+os.system("python -m spacy download en")
---
+++ news_bot/scripts/veja.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("cd news_bot; scrapy crawl veja")
